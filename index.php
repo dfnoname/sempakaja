09a0b9088b4121f4d8a98401a8dd758983b2028e
No Name <dfnoname@users.noreply.github.com>
2017-08-22 05:28:25 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,11 @@
 <?php
-//error_reporting(0);
-//@ini_set('display_errors', 0);
+error_reporting(0);
+@ini_set('display_errors', 0);
 if(!isset($_GET['url']) || empty($_GET['url'])){
 echo ":(";
-}
+	exit();
+}	
+
 
 $result= ffffffS($_GET['url']);
 
